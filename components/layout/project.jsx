e47d8a3061ff279f6a29991de9a1bafde130084f
yoga <yogaagle123z@gmail.com>
2022-08-28 15:56:07 +0700

    update

--- components/layout/project.jsx
+++ components/layout/project.jsx
@@ -1,10 +1,17 @@
-import { Heading, Box, Image, Flex, useColorModeValue } from '@chakra-ui/react'
+import {
+  Heading,
+  Box,
+  Image,
+  Flex,
+  useColorModeValue,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent
+} from '@chakra-ui/react'
 import styled from '@emotion/styled'
-import { Fragment, useState } from 'react'
-// import Section from '../section'
+import { useRef } from 'react'
 import dynamic from 'next/dynamic'
-
-// import { Dialog, Transition } from '@headlessui/react'
 
 const Section = dynamic(() => import('../section'), { ssr: false })
 
@@ -30,115 +37,34 @@ export const Mark = styled.span`
   color: ${color};
 `
 
-const Popup = styled.div`
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  padding-top: 8vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-evenly;
-`
-const Blur = styled.div`
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  background: #545454b0;
-  filter: blur(5px);
-`
-
 export const ImageProject = ({ src, alt }) => {
-  const [pop, setPop] = useState(false)
-  const [data, setData] = useState({})
-  const Val = (data, des) => {
-    setPop(true)
-    setData({ data: data, des: des })
-  }
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const finalRef = useRef(null)
   return (
     <>
-      <Flex pt={8}>
+      <Flex pt={4} alignItems="center" justifyContent={'center'}>
         <Image
-          w="full"
+          maxW="full"
           src={src}
           alt={alt}
           mb={4}
-          onClick={() => Val(src, alt)}
+          onClick={onOpen}
           cursor="pointer"
         />
       </Flex>
-      {pop ? (
-        <>
-          <Blur />
-          <Popup onClick={() => setPop(false)}>
-            <Box>
-              <Flex maxW={'60vh'} alignItems="center" justifyContent={'center'}>
-                <Image
-                  maxW={{
-                    base: ['28rem', '35rem', '35rem', '36rem'],
-                    md: ['45rem', '50rem']
-                  }}
-                  src={data.data}
-                  alt={data.des}
-                  rounded={5}
-                  objectFit="cover"
-                />
-              </Flex>
-            </Box>
-          </Popup>
-        </>
-      ) : (
-        <></>
-      )}
-    </>
-  )
-}
-export const ImageExperience = ({ src, alt }) => {
-  const [pop, setPop] = useState(false)
-  const [data, setData] = useState({})
-  const Val = (data, des) => {
-    setPop(true)
-    setData({ data: data, des: des })
-  }
-  return (
-    <>
-      <Flex pt={4}>
-        <Image
-          maxW="20rem"
-          rounded={'md'}
-          src={src}
-          alt={alt}
-          mb={4}
-          onClick={() => Val(src, alt)}
-          cursor="pointer"
-        />
-      </Flex>
-      {pop ? (
-        <>
-          <Blur />
-          <Popup onClick={() => setPop(false)}>
-            <Box>
-              <Flex maxW={'60vh'} alignItems="center" justifyContent={'center'}>
-                <Image
-                  maxW={{
-                    base: ['28rem', '35rem', '35rem', '36rem'],
-                    md: ['45rem', '50rem']
-                  }}
-                  src={data.data}
-                  alt={data.des}
-                  rounded={5}
-                  objectFit="cover"
-                />
-              </Flex>
-            </Box>
-          </Popup>
-        </>
-      ) : (
-        <></>
-      )}
+      <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(4px) " />
+        <ModalContent
+          onClick={onClose}
+          bg="none"
+          maxW="52rem"
+          h={{ base: 'full', md: 'auto' }}
+          alignItems={'center'}
+          justifyContent={'center'}
+        >
+          <Image src={src} />
+        </ModalContent>
+      </Modal>
     </>
   )
 }
@@ -155,57 +81,3 @@ export const Paragraf = ({ children }) => (
     </Box>
   </Section>
 )
-
-// const TamplateDialog = ({ children, open, setOpen, src }) => (
-//   <Transition.Root show={open} as={Fragment}>
-//     <Dialog
-//       as={'div'}
-//       className="fixed z-50 inset-0 overflow-y-auto"
-//       onClose={setOpen}
-//     >
-//       <Box
-//         display={{ base: 'flex', sm: 'block' }}
-//         alignItems={'end'}
-//         justifyContent="center"
-//         minH={{ base: '500px', sm: 'full' }}
-//         pt={{ base: 4, sm: 0 }}
-//         px={{ base: 4, sm: 0 }}
-//         pb={20}
-//         textAlign="center"
-//       >
-//         <Transition.Child
-//           as={Fragment}
-//           enter="transition ease-out duration-150"
-//           enterFrom="opacity-0"
-//           enterTo="opacity-100"
-//           leave="transition ease-in-out duration-150 transform"
-//           leaveFrom="opacity-100"
-//           leaveTo="opacity-0"
-//         >
-//           <Dialog.Overlay
-//             className={
-//               'fixed inset-0 bg-gray-700 bg-opacity-50 transition-opacity'
-//             }
-//           />
-//         </Transition.Child>
-//         <span
-//           className="hidden sm:inline-block sm:align-middle sm:h-screen"
-//           aria-hidden="true"
-//         >
-//           &#8203;
-//         </span>
-//         <Transition.Child
-//           as={Fragment}
-//           enter="transition ease-out duration-300"
-//           enterFrom="opacity-0"
-//           enterTo="opacity-100"
-//           leave="transition ease-in duration-200"
-//           leaveFrom="opacity-100"
-//           leaveTo="opacity-0"
-//         >
-//           <Image src="https://i.imgur.com/qXQQQ.png" />
-//         </Transition.Child>
-//       </Box>
-//     </Dialog>
-//   </Transition.Root>
-// )
--- pages/project/percobaan.jsx
+++ pages/project/percobaan.jsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import {
+  Box,
+  Button,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure
+} from '@chakra-ui/react'
+import React from 'react'
 
 const percobaan = () => {
-  return <div>hello</div>;
-};
+  return (
+    <div>
+      <ReturnFocus />
+    </div>
+  )
+}
 
-export default percobaan;
+export default percobaan
+
+function ReturnFocus() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const finalRef = React.useRef(null)
+
+  return (
+    <>
+      <Button mt={4} onClick={onOpen}>
+        Open Modal
+      </Button>
+      <Box h="full" alignItems={'center'} justifyContent={'center'}>
+        <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent bg={'cyan.900'} onClick={onClose}>
+            {/* <ModalHeader></ModalHeader> */}
+            {/* <ModalCloseButton /> */}
+            {/* <ModalBody> */}
+              {/* <Lorem count={2} /> */}
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDMndRG8hwzAqYRFbbLrQN7ajMZBZjTWOvRw&usqp=CAU" />
+            {/* </ModalBody> */}
+
+            {/* <ModalFooter></ModalFooter> */}
+          </ModalContent>
+        </Modal>
+      </Box>
+    </>
+  )
+}
