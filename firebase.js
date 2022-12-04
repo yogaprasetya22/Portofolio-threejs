deac473bd51b344e0907df764e059971684239cc
yoga <yogaagle123z@gmail.com>
2022-12-04 16:27:25 +0700

    update

--- firebase.js
+++ firebase.js
@@ -1,10 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from 'firebase/app'
-import { getFirestore } from '@firebase/firestore'
-import { getStorage } from '@firebase/storage'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBqdNSsSVGUaNIDnezCgLkyuhzlKcgVc-c',
